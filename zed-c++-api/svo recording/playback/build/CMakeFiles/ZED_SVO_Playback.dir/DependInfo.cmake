ad84edf310156a7c34fbf4d432e4abcefb677030
C.Sorawit <Arm_Ever00@hotmail.com>
2019-07-10 13:31:05 +0700

    Update ZED tutorial

---
+++ zed-c++-api/svo recording/playback/build/CMakeFiles/ZED_SVO_Playback.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csorawit/ZED_ws/src/svo recording/playback/src/main.cpp" "/home/csorawit/ZED_ws/src/svo recording/playback/build/CMakeFiles/ZED_SVO_Playback.dir/src/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/local/zed/include"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ zed-c++-api/svo recording/playback/build/CMakeFiles/ZED_SVO_Playback.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_SVO_Playback.dir/src/main.o"
+  "ZED_SVO_Playback.pdb"
+  "ZED_SVO_Playback"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_SVO_Playback.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
